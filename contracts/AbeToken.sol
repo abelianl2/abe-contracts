b4146c88405ee2d2ba54fb8c3c71d751d08e840b
mindcarver <626984947@qq.com>
2024-06-08 16:55:06 +0800

    perf:support EIP-2612

--- contracts/AbeToken.sol
+++ contracts/AbeToken.sol
@@ -1,14 +1,14 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.24;
 
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
-contract AbeToken is ERC20Pausable {
+contract AbeToken is ERC20Permit {
     constructor(
         string memory name_,
         string memory symbol_,
         uint256 initialSupply
-    ) ERC20(name_, symbol_) {
+    ) ERC20(name_, symbol_) ERC20Permit(name_) {
         _mint(msg.sender, initialSupply);
     }
 }
