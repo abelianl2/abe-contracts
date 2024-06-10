30940d3f4afb403238b707534b6697a1f9834fe8
mindcarver <626984947@qq.com>
2024-06-10 11:16:59 +0800

    fix:invalid type

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,17 +4,14 @@ import "./tasks/abeToken"
 import * as dotenv from 'dotenv';
 
 dotenv.config();
-const abeDAnetwork = {
-  url: "http://124.243.137.251:8888", 
-  accounts: [process.env.PRIVATE_KEY],
-  chainId: 9000, 
-};
-
-
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
   networks: {
-    abeDA: abeDAnetwork,
+    abeDA: {
+      url: "http://124.243.137.251:8888",
+      accounts: [process.env.PRIVATE_KEY || ""], // if use abeDA, you need to set your private key to .env
+      chainId: 9000,
+    },
   },
 };
 
